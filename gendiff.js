04a09ef9201dfcda307bc7e75da3835abdb4210e
elena-mb <elena.b.m@mail.ru>
2022-04-19 20:54:26 +0300

    Add gendiff.js

---
+++ gendiff.js
@@ -0,0 +1,8 @@
+const { Command } = require('commander');
+const program = new Command;
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0');
+
+program.parse();
