b97ecf6b80aeb205a8fabf8793e89394f7508d59
elena-mb <elena.b.m@mail.ru>
2022-05-13 20:56:19 +0300

    Add tests for new functionality

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,9 +6,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('shallow tree', () => {
+test('shallow tree in json', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
+  expect(genDiff(filepath1, filepath2))
+    .toEqual('{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}');
+});
+
+test('shallow tree in yaml', () => {
+  const filepath1 = getFixturePath('file1.yaml');
+  const filepath2 = getFixturePath('file2.yml');
   expect(genDiff(filepath1, filepath2))
     .toEqual('{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}');
 });
