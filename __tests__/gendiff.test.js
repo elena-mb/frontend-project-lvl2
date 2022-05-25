47c6a89c80a38fddb42ebd3e19993e769f71e700
elena-mb <elena.b.m@mail.ru>
2022-05-25 20:58:37 +0300

    Add new tests for genDiff

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,21 +1,64 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import genDiff from '../src/gendiffFn.js';
+import genDiff from '../src/genDiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('shallow tree in json', () => {
+const gendiffResult = `{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`;
+
+test('json file', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
-  expect(genDiff(filepath1, filepath2))
-    .toEqual('{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}');
+  expect(genDiff(filepath1, filepath2)).toEqual(gendiffResult);
 });
 
-test('shallow tree in yaml', () => {
+test('yaml file', () => {
   const filepath1 = getFixturePath('file1.yaml');
   const filepath2 = getFixturePath('file2.yml');
-  expect(genDiff(filepath1, filepath2))
-    .toEqual('{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true\n}');
+  expect(genDiff(filepath1, filepath2)).toEqual(gendiffResult);
 });
