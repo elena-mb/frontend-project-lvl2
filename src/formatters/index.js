a73876c53ae39cd6d7b0ad08731035d36f6d0de0
elena-mb <elena.b.m@mail.ru>
2022-05-30 21:32:11 +0300

    Add new feature (--format json)

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,6 +1,14 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js';
 
-export default (ast, data1, data2, formatStyle) => (formatStyle === 'stylish'
-  ? stylish(ast, data1, data2)
-  : plain(ast, data1, data2));
+export default (ast, data1, data2, formatStyle) => {
+  switch (formatStyle) {
+    case 'plain':
+      return plain(ast, data1, data2);
+    case 'json':
+      return json(ast, data1, data2);
+    default:
+      return stylish(ast, data1, data2);
+  }
+};
